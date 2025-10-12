de140f6128b0014659004a1cf05911e637b34c43
GIOVANNI ILACQUA S338884 <s338884@studenti.polito.it>
2025-10-12 17:26:28 +0200

    service ORM

---
+++ src/models/dao/serviceDAO.ts
@@ -0,0 +1,26 @@
+import {
+    Entity,
+    Column,
+    OneToMany,
+    PrimaryGeneratedColumn,
+} from "typeorm";
+import { TicketDAO } from "@models/dao/ticketDAO";
+
+@Entity("service")
+export class ServiceDAO {
+    @PrimaryGeneratedColumn()
+    id_service: number;
+
+    @Column({ type: "varchar", unique: true })
+    name: string;
+
+    @Column({ type: "text", nullable: true })
+    description: string | null;
+
+    @OneToMany(
+        () => TicketDAO,
+        (ticket) => ticket.service,
+        { cascade: true }
+    )
+    tickets: TicketDAO[] = [];
+}
