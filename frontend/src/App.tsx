4f6a5d65f686d1e879efffa6828201b33eb9bd77
Rutu0705 <82505918+Rutu0705@users.noreply.github.com>
2025-10-15 16:39:49 +0200

    Create BoardPage for customer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { Routes, Route, Navigate } from 'react-router';
 import HomePage from './components/Home';
 import SList from './components/ServiceList';
+import BoardPage from './components/BoardPage';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/services" element={<SList />} />
+      <Route path="/board" element={<BoardPage />} />
     </Routes>
   )
 }
+
+
 
 export default App
