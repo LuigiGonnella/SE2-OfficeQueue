e797076e310b00cad33daafd035a168c14e82786
EAnzaldi <elisabettaanzaldi1@gmail.com>
2025-10-15 16:25:18 +0200

    Centered Service List

--- frontend/src/components/ServiceList.tsx
+++ frontend/src/components/ServiceList.tsx
@@ -1,18 +1,22 @@
-import {Col, Row, Button, Card, ListGroup} from "react-bootstrap";
+import {Col, Row, Button, Card, ListGroup, Container} from "react-bootstrap";
 import { Link, useNavigate } from "react-router";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {DefaultApiAxiosParamCreator} from "../API/api.ts";
 
 function SList() {
+    
     return (
-        <>
+
+        
+        <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
             <Card style={{ width: '18rem' }}>
                 <ListGroup variant="flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item>Service 1</ListGroup.Item>
+                <ListGroup.Item>Service 2</ListGroup.Item>
+                <ListGroup.Item>Service 3</ListGroup.Item>
                 </ListGroup>
             </Card>
-        </>
+        </Container>
     )
 }
 export default SList;
