a498e93b96420c0c4b409d14af67e94becf0d858
EAnzaldi <elisabettaanzaldi1@gmail.com>
2025-10-15 14:10:38 +0200

    Added React Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dropdown from 'react-bootstrap/Dropdown';
+
+function DropdownExample() {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
